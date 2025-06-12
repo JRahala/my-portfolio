7de5eb78753765ea795b2c77b2eb7b47d09a29c1
JRahala <jr3459@princeton.edu>
2025-06-12 17:56:02 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/my-portfolio/", // since this is on my github repo
   plugins: [react()],
 })
